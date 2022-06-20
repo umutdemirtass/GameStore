683c0f036a0f0469e0c59d696706715177030c76
umutdemirtass <umut.demirtas@ankabeta.com>
2022-06-20 22:24:49 +0300

    routers created

--- GameStore/src/app/app-routing.module.ts
+++ GameStore/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameCreationComponent } from './pages/game-creation/game-creation.component';
+import { GameListComponent } from './pages/game-list/game-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:GameListComponent
+  },
+  {
+    path:'game-create',
+    component:GameCreationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GameStore/src/app/app.module.ts
+++ GameStore/src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameCreationComponent } from './pages/apps/game-creation/game-creation.component';
-import { GameListComponent } from './pages/apps/game-list/game-list.component';
-import { AppsComponent } from './pages/apps/apps.component';
+import { GameCreationComponent } from './pages/game-creation/game-creation.component';
+import { GameListComponent } from './pages/game-list/game-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameCreationComponent,
     GameListComponent,
-    AppsComponent
+    
   ],
   imports: [
     BrowserModule,
