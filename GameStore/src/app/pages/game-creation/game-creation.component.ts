51174ad923173d7b2c054ce929cbdca4e6fb92cf
umutdemirtass <umut.demirtas@ankabeta.com>
2022-06-21 00:12:03 +0300

    test form

--- GameStore/src/app/pages/game-creation/game-creation.component.ts
+++ GameStore/src/app/pages/game-creation/game-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-game-creation',
@@ -11,5 +12,12 @@ export class GameCreationComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  createGame = new FormGroup({
+    gameName: new FormControl(''),
+    bundle: new FormControl(''),
+    owner:new FormControl('')
+  })
+  created(){
+    console.log(this.createGame.value)
+  }
 }
