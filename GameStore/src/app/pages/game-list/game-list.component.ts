d1957eaf9d4fa0c912ffe2d03222c624b53df584
umutdemirtass <umut.demirtas@ankabeta.com>
2022-06-22 12:33:12 +0300

    game-list page changes

--- GameStore/src/app/pages/game-list/game-list.component.ts
+++ GameStore/src/app/pages/game-list/game-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { combineLatest, map } from 'rxjs';
+import { Game } from 'src/app/game';
+import { deleteGame } from 'src/app/store/game.action';
+import { gameSelector } from 'src/app/store/game.selector';
 
 @Component({
   selector: 'app-game-list',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.scss']
 })
 export class GameListComponent implements OnInit {
+  show: boolean = false
 
-  constructor() { }
+  games$ = this.store.select(gameSelector);
+  searchControl = new FormControl();
+
+  // search kontrol icin combineLatest kullandim. iki observable kontorlu icin
+  filteredGames$ = combineLatest([this.games$, this.searchControl.valueChanges]).pipe(
+    map(([gameArr, filterValue]) => {
+      if (filterValue)
+        return gameArr.filter((e: Game) => e.gameName.startsWith(filterValue));
+      else
+        return gameArr;
+    })
+  )
+
+  filteredGames: Game[] = [];
+
+  constructor(
+    private store: Store
+  ) { }
+
+
+  deleteGame(game: any) {
+    let confirmAlert = confirm('Are you sure you want to delete the game?');
+    if (confirmAlert) this.store.dispatch(deleteGame({ game }))
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.searchControl.setValue('')
+    }, 100);
   }
 
 }
